7e417b630937da5f0d5d5bc7c3e0f5f1500b9a85
presley <presley.mcgown@icloud.com>
2023-02-09 09:10:27 -0700

    character model pt 1

---
+++ src/CharacterClass.java
@@ -0,0 +1,2 @@
+public class CharacterClass {
+}
---
+++ src/CharacterSheet.java
@@ -0,0 +1,31 @@
+public class CharacterSheet {
+
+    // * basic info
+    public String characterName;
+    public CharacterClass characterClass;
+    public int level;
+    public Race characterRace;
+    public Background background;
+    public Alignment alignment;
+    public String playerName;
+    public int xp;
+
+    // * hit-points and armor
+    public int armorClass;
+    public int initiative;
+    public int speed;
+    public int maxHP;
+    public int hitDiceType;
+    public int amountOfHitDice;
+    public int proficiencyBonus;
+
+    // * ability scores
+    public int strength;
+    public int dexterity;
+    public int contitution;
+    public int intelligent;
+    public int wisdom;
+    public int charisma;
+    public int passivePerception;
+
+}
